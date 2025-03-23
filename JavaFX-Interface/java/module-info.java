37664339bed402cbef60b911c5f524f624e27b40
Leti-10 <leti.oli.lopes@gmail.com>
2025-03-23 16:40:18 -0300

    add a interface
    vai precisar ser reorganizado todos os códigos para serem integrados juntos, mas eu não consigo fazer :(

---
+++ JavaFX-Interface/java/module-info.java
@@ -0,0 +1,15 @@
+module org.sputnik.api {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires java.desktop;
+
+    opens org.sputnik.api to javafx.fxml;
+    exports org.sputnik.api;
+}
