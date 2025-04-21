bf7b51f07c1547f72a4818120e2815d5a31bbbcd
Leti-10 <leti.oli.lopes@gmail.com>
2025-04-21 11:38:41 -0300

    add-- module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module org.sputnik.api {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires javafx.base;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires ollama4j;
+    requires reactfx;
+    requires java.desktop;
+    requires org.fxmisc.richtext;
+
+    opens org.sputnik.api to javafx.fxml;
+    exports org.sputnik.api;
+}
