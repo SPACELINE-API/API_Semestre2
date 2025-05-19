f772fc1a4e32baa0d4ab363f944c994db62be9b1
Leti-10 <leti.oli.lopes@gmail.com>
2025-05-19 19:15:16 -0300

    logo!

--- src/main/java/org/sputnik/api/Main.java
+++ src/main/java/org/sputnik/api/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javax.swing.*;
@@ -19,6 +20,7 @@ public class Main extends Application {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("scene-builder.fxml")));
             Scene scene = new Scene(root);
             scene.getStylesheets().add(getClass().getResource("/Css/principal.css").toExternalForm());
+            stage.getIcons().add(new Image(getClass().getResourceAsStream("/Css/imagem/satelite com fundo.png")));
             stage.initStyle(StageStyle.TRANSPARENT);
             stage.setScene(scene);
             stage.show();
